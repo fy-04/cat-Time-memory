e7209c92424c87f3a6a408bb56f6cf53eac6b8e0
fy-04 <413648812@qq.com>
2025-06-23 19:05:33 +0800

    fix: 全局修改格式

--- src/main.js
+++ src/main.js
@@ -2,14 +2,9 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "@/assets/global.css";
 
 Vue.config.productionTip = false;
-
-// TODO: 改成引入文件，不要用 style 标签做 =>  global.css
-const style = document.createElement("style");
-style.textContent = `body, html { margin: 0; padding: 0; font-family: sans-serif; } * { box-sizing: border-box; }`;
-
-document.head.appendChild(style);
 
 new Vue({
   router,
